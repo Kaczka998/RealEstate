950410b2b1054e8371456e6c46cc46741ebd70a6
Kaczka998 <weronika.wolszczak@gmail.com>
2022-10-19 14:22:33 +0200

    changes and improvements

--- force-app/main/default/staticresources/leadFromController.js
+++ force-app/main/default/staticresources/leadFromController.js
@@ -1,25 +1,26 @@
     (function () {
-      document.getElementById("companyContainer").style.display = "none";
+      document.getElementById("companyC").style.display = "none";
     })();
 
-    const selectElement = document.getElementById("00N7Q00000J5gwH");
+    const selectElement = document.getElementById("00N7Q00000DXaY9");
 
     selectElement.addEventListener('change', (event) => {
       if (event.target.value == 'Business Premises') {
         document.getElementById("company").value = "";
-        document.getElementById("companyContainer").style.display = "block";
-        document.getElementById("recordType").value = '0127Q000001fzuZQAQ';
+        document.getElementById("companyC").style.display = "block";
+        document.getElementById("recordType").value = '0127Q000000EsLoQAK';
+    
         
       }
-      else if (event.target.value == 'Flats And Apartements') {
-        document.getElementById("companyContainer").style.display = "none";
-        document.getElementById("recordType").value = '0127Q000001fzueQAA';
+      else if (event.target.value == 'Flats' || event.target.value == 'Apartments') {
+        document.getElementById("companyC").style.display = "none";
+        document.getElementById("recordType").value = '0127Q000000EsLyQA';
         document.getElementById("company").value = "-";
       }
     });
 
 function validateInputs(){
-  console.log('vlidated')
+  console.log(document.getElementById("recordType").value)
 }
 
     function submitForm( ){
