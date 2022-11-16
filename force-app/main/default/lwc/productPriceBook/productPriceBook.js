5429bcc669415b79f09ae0a5403a751851481fd4
Kaczka998 <weronika.wolszczak@gmail.com>
2022-11-16 16:42:20 +0100

    update github

---
+++ force-app/main/default/lwc/productPriceBook/productPriceBook.js
@@ -0,0 +1,27 @@
+// import { LightningElement, api, wire, track } from "lwc";
+// import getRecordsByRecordType from "@salesforce/apex/RE_productController.getRecordsByRecordType";
+
+// const columns = [
+//   { label: "Premise Name", fieldName: "Name", editable: false },
+//   { label: "Price", fieldName: "UnitPrice", type: "currency", typeAttributes:{currencyCode: "USD"}, editable: false }
+// ];
+
+export default class ProductPriceBook extends LightningElement {
+  // @api objectApiName;
+  // @api recordType;
+
+  // @track productsInfo;
+
+  // columns = columns;
+
+  // @wire(getRecordsByRecordType, { premiseName: "Business premises" })
+  // wiredResult({ data, error }) {
+  //   if (data) {
+  //     console.log(data);
+  //     this.productsInfo = data;
+  //   }
+  //   if (error) {
+  //     console.log(error);
+  //   }
+  // }
+}
--- force-app/main/default/lwc/propertiesResultList/propertiesResultList.js
+++ force-app/main/default/lwc/propertiesResultList/propertiesResultList.js
@@ -1,7 +1,46 @@
-import { LightningElement, api} from 'lwc';
+import { LightningElement, api, track } from 'lwc';
 
 export default class PropertiesResultList extends LightningElement {
 
 @api propertyList;
+@api listSize;
 
+@track pageData = [];
+
+pageNumber = 0;
+allData=[];
+
+    connectedCallback(){
+        this.updatePage(); 
+    }
+
+    updatePage(){
+        this.allData = [];
+        this.propertyList.forEach(element => 
+            this.allData.push(element)
+            );
+       let records = Math.min(this.allData.length, 5);
+       this.pageData = this.allData.slice(this.pageNumber, records);
+       for(let i=0; i<2; i++){
+        console.log(this.pageData[i]);
+       }
+    }
+
+    first() {
+        this.pageNumber = 0;
+        this.updatePage()
+      }
+
+    last() {
+        this.pageNumber = Math.floor((this.propertyList.length-4)/5)
+        this.updatePage()
+    }
+    previous() {
+        this.pageNumber = Math.max(0, this.pageNumber - 1)
+        this.updatePage()
+    }
+    next() {
+        this.pageNumber = Math.min(Math.floor((this.allData.length-4)/5), this.pageNumber + 1)
+        this.updatePage()
+      }
 }
